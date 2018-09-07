b19d6f5cf386bc9dbc5d72e9cbc1859008d69da1
kobayashi-m42 <m42kobayashi@gmail.com>
2018-09-07 18:46:16 +0900

    コントローラにアクションを追加

--- app/Http/Controllers/TodosController.php
+++ app/Http/Controllers/TodosController.php
@@ -3,8 +3,77 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Todo;
 
 class TodosController extends Controller
 {
-    //
+    /**
+     * Todoの一覧を取得する
+     *
+     * @return JsonResponse
+     */
+    function index(): JsonResponse
+    {
+        $todos = Todo::all();
+        return response()->json([$todos]);
+    }
+
+    /**
+     * Todoを登録する
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    function store(Request $request): JsonResponse
+    {
+        $todo = new Todo();
+        $todo->title = $request->title;
+        $todo->state = $request->state;
+        $todo->save();
+        return response()->json();
+    }
+
+    /**
+     * Todoを取得する
+     *
+     * @param Request $request
+     * @param string $id
+     * @return JsonResponse
+     */
+    function show(Request $request, string $id): JsonResponse
+    {
+        $todo = Todo::find($id);
+        return response()->json($todo);
+    }
+
+    /**
+     * Todoを更新する
+     *
+     * @param Request $request
+     * @param string $id
+     * @return JsonResponse
+     */
+    function update(Request $request, string $id): JsonResponse
+    {
+        $todo = Todo::find($id);
+        $todo->title = $request->title;
+        $todo->state = $request->state;
+        $todo->save();
+        return response()->json();
+    }
+
+    /**
+     * Todoを削除する
+     *
+     * @param Request $request
+     * @param string $id
+     * @return JsonResponse
+     */
+    function destroy(Request $request, string $id): JsonResponse
+    {
+        $todo = ToDo::find($id);
+        $todo->delete();
+        return response()->json();
+    }
 }
